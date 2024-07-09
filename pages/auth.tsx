59c07e91263c45f72c28edeac829d0337bd0917e
reza <rezakurnia1902@gmail.com>
2024-07-09 20:25:26 +0700

    hide github google login because have some bug

--- pages/auth.tsx
+++ pages/auth.tsx
@@ -81,14 +81,14 @@ const Auth = () => {
             >
               {variant === "login" ? "Login" : "Sign up"}
             </button>
-            <div className='flex flex-col items-center gap-4 mt-8 justify-center'>
+            {/* <div className='flex flex-col items-center gap-4 mt-8 justify-center'>
               <div
                 onClick={() => signIn("github", { callbackUrl: "/profile" })}
                 className='w-10 h-10 bg-white rounded-full flex items-center justify-center cursor-pointer hover:opacity-80 transition'
               >
                 <FaGithub size={30} />
               </div>
-            </div>
+            </div> */}
             <p className='text-neutral-500 mt-12'>
               {variant === "login"
                 ? "First time using Netflix?"
